f54c16980f0bfe4b5babea65f7fc815a85bf4fa7
Mr. Mueller <bmueller@hstat.org>
2025-05-01 14:18:30 -0400

    keyboard movement

--- sumo/sketch.js
+++ sumo/sketch.js
@@ -1,60 +1,55 @@
 let q = await Q5.WebGPU();
 
-new Canvas();
-// world.gravity.y = 10;
+new Canvas(1000,800);
 world.gravity.y = 0;
 
-// let ball = new Sprite();
-// ball.x = -200;
-// ball.y = -200;
-// ball.diameter = 50;
-
-// let groundA = new Sprite();
-// groundA.x = -120;
-// groundA.width = 200;
-// groundA.rotation = 20;
-// groundA.collider = STATIC;
-
-// let groundB = new Sprite();
-// groundB.x = 120;
-// groundB.width = 200;
-// groundB.rotation = -20;
-// groundB.collider = STATIC;
-
 let myPlayer;
+let otherPlayer;
 let players = {};
 let circleRadius = 300;
 let centerX = 400, centerY = 300;
 
-myPlayer = new Sprite(random(width), random(height), 30);
-myPlayer.color = 'orange';
+myPlayer = new Sprite(width/2, height/2, 30);
+myPlayer.color = 'red';
+
+otherPlayer = new Sprite(random(width), random(height), 30);
+otherPlayer.color = 'blue';
 
 
 q.update = () => {
 	background('skyblue');
 
-	// textAlign(CENTER);
-	// text('click to jump!', 0, -100);
-
-	// if (mouse.presses()) ball.vel.y = -5;
-	// kb.presses(' ')
-
-	// if (kb.presses('left')) myPlayer.vel.x = -5;
-	// if (kb.presses('right')) myPlayer.vel.x = 5;
-	// if (kb.presses('up')) myPlayer.vel.y = -5;
-	// if (kb.presses('down')) myPlayer.vel.y = 5;
+	otherPlayer.x = mouse.x;
+	otherPlayer.y = mouse.y;
 
 	if (kb.pressing('up')) {
+		myPlayer.vel.x = 0;
 		myPlayer.vel.y = -5;
 	} else if (kb.pressing('down')) {
+		myPlayer.vel.x = 0;
 		myPlayer.vel.y = 5;
 	} else if (kb.pressing('left')) {
 		myPlayer.vel.x = -5;
+		myPlayer.vel.y = 0;
 	} else if (kb.pressing('right')) {
 		myPlayer.vel.x = 5;
+		myPlayer.vel.y = 0;
 	} else {
 		myPlayer.vel.x = 0;
 		myPlayer.vel.y = 0;
+	}
+
+	if (kb.pressing('w')) { // up
+		otherPlayer.vel.y = -5;
+	} else if (kb.pressing('s')) { // down
+		otherPlayer.vel.y = 5;
+	} else if (kb.pressing('a')) { // left
+		otherPlayer.vel.x = -5;
+	} else if (kb.pressing('d')) { // right
+		otherPlayer.vel.x = 5;
+	} else {
+		otherPlayer.vel.x = 0;
+		otherPlayer.vel.y = 0;
 	}
 };
 
